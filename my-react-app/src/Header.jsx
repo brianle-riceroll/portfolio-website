e278dd4ab13a75633889354866d4fa5b24a72d35
Brian Le <brianle17360@gmail.com>
2024-06-22 12:06:55 -0700

    Added gradient background and fixed image and text position

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -1,7 +1,13 @@
+import image from './assets/8bit_me_transparent.png'
+
 function Header(prop) {
     return(
         <>
-            <h1 className="Title">{prop.name}</h1>
+            <div className="header-div">
+                <img src={image} alt="8bit image" height="100" className="_8bit-img"></img>
+                <h1 className="title">{prop.name}</h1>
+            </div>
+            <hr></hr>
         </>
     ) 
 }
