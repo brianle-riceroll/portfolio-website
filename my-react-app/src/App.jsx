5d8875053eee341f6903c55f020ab983f1b1f525
Brian Le <bpl4@pdx.edu>
2024-06-22 01:12:05 -0700

    first commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './Header.jsx'
+
+function App() {
+  return (
+    <>
+      <Header name="Brian Le"></Header> 
+    </>
+  );
+}
+
+export default App
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,9 @@
+function Header(prop) {
+    return(
+        <>
+            <h1 className="Title">{prop.name}</h1>
+        </>
+    ) 
+}
+
+export default Header
